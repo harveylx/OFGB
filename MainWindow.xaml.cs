5a1508a437401e15f26e5b241d5f1d878b03f9a4
Harvey Singh <harvirjaswal@gmail.com>
2024-05-25 23:54:24 +0100

    Moved Dictionaries into concrete class called RegistryEntry

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows.Interop;
 using System.Runtime.InteropServices;
 using System.Windows.Controls;
 using System.Security.Principal;
-using System.Windows.Input;
 
 namespace OFGB;
 
@@ -19,16 +18,59 @@ public partial class MainWindow : Window
     internal static partial int DwmSetWindowAttribute(IntPtr hwnd, int attr, [In] int[] attrValue, int attrSize);
 
     private const string CurVer = @"Software\Microsoft\Windows\CurrentVersion\";
-    private static readonly string[] Cb1KeyNames = { "ShowSyncProviderNotifications" };
-    private static readonly string[] Cb2KeyNames = { "RotatingLockScreenOverlayEnabled", "SubscribedContent-338387Enabled" };
-    private static readonly string[] Cb3KeyNames = { "SubscribedContent-338393Enabled", "SubscribedContent-353694Enabled", "SubscribedContent-353696Enabled" };
-    private static readonly string[] Cb4KeyNames = { "SubscribedContent-338389Enabled" };
-    private static readonly string[] Cb5KeyNames = { "ScoobeSystemSettingEnabled" };
-    private static readonly string[] Cb6KeyNames = { "SubscribedContent-310093Enabled" };
-    private static readonly string[] Cb7KeyNames = { "Enabled" };
-    private static readonly string[] Cb8KeyNames = { "TailoredExperiencesWithDiagnosticDataEnabled" };
-    private static readonly string[] Cb9KeyNames = { "Start_IrisRecommendations" };
-    private static readonly string[] Cb10KeyNames = { "Enabled" };
+
+    private static readonly List<RegistryEntry> Cb1RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + @"Explorer\Advanced", "ShowSyncProviderNotifications")
+    };
+    private static readonly List<RegistryEntry> Cb2RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "RotatingLockScreenOverlayEnabled"),
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "SubscribedContent-338387Enabled")
+    };
+    private static readonly List<RegistryEntry> Cb3RegistryEntries = new ()
+    {
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "SubscribedContent-338393Enabled"),
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "SubscribedContent-353694Enabled"),
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "SubscribedContent-353696Enabled")
+    };
+    private static readonly List<RegistryEntry> Cb4RegistryEntries = new ()
+    {
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "SubscribedContent-338389Enabled")
+    };
+    private static readonly List<RegistryEntry> Cb5RegistryEntries = new ()
+    {
+        new RegistryEntry(CurVer + "UserProfileEngagement", "ScoobeSystemSettingEnabled")
+    };
+    private static readonly List<RegistryEntry> Cb6RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + "ContentDeliveryManager", "SubscribedContent-310093Enabled")
+    };
+    private static readonly List<RegistryEntry> Cb7RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + "AdvertisingInfo", "Enabled")
+    };
+    private static readonly List<RegistryEntry> Cb8RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + "Privacy", "TailoredExperiencesWithDiagnosticDataEnabled")
+    };
+    private static readonly List<RegistryEntry> Cb9RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + @"Explorer\Advanced", "Start_IrisRecommendations")
+    };
+    private static readonly List<RegistryEntry> Cb10RegistryEntries = new()
+    {
+        new RegistryEntry(CurVer + @"Notifications\Settings\Windows.ActionCenter.SmartOptOut", "Enabled")
+    };
+    private static readonly List<RegistryEntry> Cb11RegistryEntries = new()
+    {
+        new RegistryEntry(@"Software\Policies\Microsoft\Windows\Explorer\", "DisableSearchBoxSuggestions", true),
+        new RegistryEntry(CurVer + "Search", "BingSearchEnabled")
+    };
+    private static readonly List<RegistryEntry> Cb12RegistryEntries = new()
+    {
+        new RegistryEntry(@"Software\Policies\Microsoft\Edge", "WebWidgetAllowed")
+    };
 
     public MainWindow()
     {
@@ -45,46 +87,43 @@ public partial class MainWindow : Window
     private void InitialiseKeys()
     {
         // Sync provider notifications in File Explorer
-        SetCheckBoxState(cb1, CurVer + @"Explorer\Advanced", Cb1KeyNames);
+        SetCheckBoxState(cb1, Cb1RegistryEntries);
 
         // Get fun facts, tips, tricks, and more on your lock screen
-        SetCheckBoxState(cb2, CurVer + "ContentDeliveryManager", Cb2KeyNames);
+        SetCheckBoxState(cb2, Cb2RegistryEntries);
 
         // Show suggested content in Settings app
-        SetCheckBoxState(cb3, CurVer + "ContentDeliveryManager", Cb3KeyNames);
+        SetCheckBoxState(cb3, Cb3RegistryEntries);
 
         // Get tips and suggestions when using Windows
-        SetCheckBoxState(cb4, CurVer + "ContentDeliveryManager", Cb4KeyNames);
+        SetCheckBoxState(cb4, Cb4RegistryEntries);
 
         // Suggest ways to get the most out of Windows and finish setting up this device
-        SetCheckBoxState(cb5, CurVer + "UserProfileEngagement", Cb5KeyNames);
+        SetCheckBoxState(cb5, Cb5RegistryEntries);
 
         // Show me the Windows welcome experience after updates and occasionally when I sign in to highlight what's new and suggested
-        SetCheckBoxState(cb6, CurVer + "ContentDeliveryManager", Cb6KeyNames);
+        SetCheckBoxState(cb6, Cb6RegistryEntries);
 
         // Let apps show me personalized ads by using my advertising ID
-        SetCheckBoxState(cb7, CurVer + "AdvertisingInfo", Cb7KeyNames);
+        SetCheckBoxState(cb7, Cb7RegistryEntries);
 
         // Tailored experiences
-        SetCheckBoxState(cb8, CurVer + "Privacy", Cb8KeyNames);
+        SetCheckBoxState(cb8, Cb8RegistryEntries);
 
         // "Show recommendations for tips, shortcuts, new apps, and more" on Start
-        SetCheckBoxState(cb9, CurVer + @"Explorer\Advanced", Cb9KeyNames);
+        SetCheckBoxState(cb9, Cb9RegistryEntries);
 
         // "Turn off notifications from <app>? We noticed you haven't opened these in a while."
-        SetCheckBoxState(cb10, CurVer + @"Notifications\Settings\Windows.ActionCenter.SmartOptOut", Cb10KeyNames);
+        SetCheckBoxState(cb10, Cb10RegistryEntries);
 
         // These Need To Be Run As Administrator
         if (IsRunningAsAdministrator())
         {
             // Show Bing Results in Windows Search (Inverted, 1 == Disabled)
-            var key14 = CreateKey(@"Software\Policies\Microsoft\Windows\Explorer", "DisableSearchBoxSuggestions");
-            var key15 = CreateKey(CurVer + "Search", "BingSearchEnabled");
-            cb11.IsChecked = !key14 && key15;
+            SetCheckBoxState(cb11, Cb11RegistryEntries);
 
             // Disable Edge desktop search widget bar
-            var key16 = CreateKey(@"Software\Policies\Microsoft\Edge", "WebWidgetAllowed");
-            cb12.IsChecked = key16;
+            SetCheckBoxState(cb12, Cb12RegistryEntries);
         }
         else
         {
@@ -93,53 +132,27 @@ public partial class MainWindow : Window
         }
     }
 
-    private static void SetCheckBoxState(CheckBox checkBox, string keyPath, IEnumerable<string> keyNames, bool keyValueInverted = false)
+    private static void SetCheckBoxState(CheckBox checkBox, IEnumerable<RegistryEntry> registryEntries)
     {
-        var keyState = true;
-        foreach (var keyName in keyNames)
+        var keyDisabled = true;
+        foreach (var registryEntry in registryEntries)
         {
-            keyState &= GetKeyState(keyPath, keyName);
+            keyDisabled &= IsKeyDisabled(registryEntry);
         }
-        checkBox.IsChecked = keyState;
+        checkBox.IsChecked = keyDisabled;
     }
 
-    private static bool GetKeyState(string keyPath, string keyName, bool keyValueInverted = false)
+    private static bool IsKeyDisabled(RegistryEntry registryEntry)
     {
-        using var keyRef = Registry.CurrentUser.OpenSubKey(keyPath, true) ?? Registry.CurrentUser.CreateSubKey(keyPath);
+        using var keyRef = Registry.CurrentUser.OpenSubKey(registryEntry.KeyPath, true) ?? Registry.CurrentUser.CreateSubKey(registryEntry.KeyPath);
         if (keyRef == null)
         {
             MessageBox.Show("Failed to create a registry subkey during initialization!", "OFGB: Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
             throw new InvalidOperationException("OFGB: Failed to create subkey during initialization!");
         }
 
-        var value = Convert.ToInt32(keyRef.GetValue(keyName, 0));
-        return keyValueInverted ? value != 0 : value == 0;
-    }
-
-    private static bool CreateKey(string loc, string key)
-    {
-        RegistryKey? keyRef;
-
-        if (Registry.CurrentUser.OpenSubKey(loc, true) is not null)
-        {
-            keyRef = Registry.CurrentUser.OpenSubKey(loc, true);
-        }
-        else
-        {
-            keyRef = Registry.CurrentUser.CreateSubKey(loc);
-            keyRef.SetValue(key, 0);
-        }
-
-        if (keyRef is null)
-        {
-            MessageBox.Show("Failed to create a registry subkey during initialization!", "OFGB: Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            throw new InvalidOperationException("OFGB: Failed to create subkey during initialization!");
-        }
-
-        var value = Convert.ToInt32(keyRef.GetValue(key));
-        keyRef.Close();
-
-        return value == 0;
+        var value = Convert.ToInt32(keyRef.GetValue(registryEntry.KeyName, 0));
+        return registryEntry.ValueInverted ? value != 0 : value == 0;
     }
 
     private static void ToggleOptions(string checkboxName, bool enable)
---
+++ RegistryEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace OFGB;
+
+public class RegistryEntry(string keyPath, string keyName, bool valueInverted = false)
+{
+    public string KeyPath { get; } = keyPath;
+    public string KeyName { get; } = keyName;
+    public bool ValueInverted { get; } = valueInverted;
+}
